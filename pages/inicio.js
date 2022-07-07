30d736873464658e1ebb3b58c21e4b8cc6079ecc
Joaquin Guzman <joaquinguzman282@gmail.com>
2022-07-07 20:10:44 -0300

    state de conceptos y cursos

--- pages/inicio.js
+++ pages/inicio.js
@@ -12,7 +12,6 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  useDisclosure,
 } from "@chakra-ui/react";
 import Tabla from "react-data-table-component";
 const Inicio = () => {
@@ -22,18 +21,25 @@ const Inicio = () => {
   const [descripcionConcepto, setDescripcionConcepto] = useState("");
   const [fechaConcepto, setFechaConcepto] = useState("");
   const [datosConceptos, setDatosConceptos] = useState([]);
+
+  const [datosCursos, setDatosCursos] = useState([]);
+  const [codigo, setCodigo] = useState("");
+  const [nombreCurso, setNombreCurso] = useState("");
+  const [nombreSecCurso, setNombreSecCurso] = useState("");
+  const [cupoMaximo, setCupoMaximo] = useState(0);
+  const [emailResp, setEmailResp] = useState("");
   const [error, setError] = useState(false);
   const columnasConceptos = [
     {
-      name: "NOMBRE ",
+      name: "Nombre ",
       selector: (row) => row.nombre,
     },
     {
-      name: "DESCRIPCION",
+      name: "Descripcion",
       selector: (row) => row.descripcion,
     },
     {
-      name: "FECHA",
+      name: "Fecha de inicio",
       selector: (row) => row.fecha,
     },
     {
@@ -53,27 +59,24 @@ const Inicio = () => {
   ];
   const columnasCursos = [
     {
-      name: "NOMBRE ",
-      selector: (row) => row.nombre,
+      name: "Código",
+      selector: (row) => row.codigo,
     },
     {
-      name: "DESCRIPCION",
-      selector: (row) => row.descripcion,
-    },
-  ];
-
-  const datosCursos = [
-    {
-      id: 1,
-      nombre: "Estadistica aplicada a la genética forense PUBLICO-GENERAL",
-      descripcion: "Destinado a medicos, biólogos y publico en general",
-      fecha: "10/02/2022",
+      name: "Nombre Curso ",
+      selector: (row) => row.nombreCurso,
     },
     {
-      id: 2,
-      nombre: "Estadistica aplicada a la genética forense DOCENTES",
-      descripcion: "dedicado solo a docentes",
-      fecha: "14/05/2022",
+      name: "Nombre Secundario",
+      selector: (row) => row.nombreSecCurso,
+    },
+    {
+      name: "Cupo Maximo",
+      selector: (row) => row.cupoMaximo,
+    },
+    {
+      name: "Email  Responsable",
+      selector: (row) => row.emailResp,
     },
   ];
 
@@ -91,14 +94,44 @@ const Inicio = () => {
       setDatosConceptos([...datosConceptos, objetoConceptos]);
     }
   };
-  console.log("datos:", datosConceptos);
+
+  const agregarCurso = () => {
+    console.log("Ingreso a la funcion");
+    if (
+      [codigo, nombreCurso, nombreSecCurso, cupoMaximo, emailResp].includes("")
+    ) {
+      setError(true);
+      console.log("Hay un error");
+    } else {
+      console.log("No hay error");
+      setModalCursos(false);
+      setError(false);
+      const objetoCursos = {
+        codigo: codigo,
+        nombreCurso: nombreCurso,
+        nombreSecCurso: nombreSecCurso,
+        cupoMaximo: cupoMaximo,
+        emailResp: emailResp,
+      };
+      setDatosCursos([...datosCursos, objetoCursos]);
+    }
+  };
   return (
     <>
       <Box mt={3} w="80%" mx="auto" border="solid 1px" p={3}>
-        <Tabla mt={2} columns={columnasConceptos} data={datosConceptos} />
+        <Tabla
+          highlightOnHover
+          pointerOnHover
+          mt={2}
+          title="Conceptos"
+          columns={columnasConceptos}
+          data={datosConceptos}
+        />
 
         <Box>
           <Button
+            mt={2}
+            mb={2}
             onClick={() => {
               setModalConceptos(true);
             }}
@@ -109,8 +142,13 @@ const Inicio = () => {
             Agregar concepto
           </Button>
         </Box>
-
-        <Tabla columns={columnasCursos} data={datosCursos} />
+        <Tabla
+          highlightOnHover
+          pointerOnHover
+          title="Cursos"
+          columns={columnasCursos}
+          data={datosCursos}
+        />
       </Box>
 
       <Modal
@@ -121,12 +159,86 @@ const Inicio = () => {
       >
         <ModalOverlay />
         <ModalContent>
+          <ModalHeader>Concepto/Curso</ModalHeader>
+          <ModalCloseButton />
           <ModalBody>
-            <p>prueba cursos</p>
+            <FormControl>
+              <FormLabel>Código</FormLabel>
+              <Input
+                size="sm"
+                name="codigo"
+                value={codigo}
+                onChange={(e) => {
+                  setCodigo(e.target.value);
+                }}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Nombre Principal</FormLabel>
+              <Input
+                size="sm"
+                name="nombreCurso"
+                value={nombreCurso}
+                onChange={(e) => {
+                  setNombreCurso(e.target.value);
+                }}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Nombre Secundario</FormLabel>
+              <Input
+                size="sm"
+                name="nombreSecCurso"
+                value={nombreSecCurso}
+                onChange={(e) => {
+                  setNombreSecCurso(e.target.value);
+                }}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Cupo Máximo</FormLabel>
+              <Input
+                size="sm"
+                name="cupoMaximo"
+                value={cupoMaximo}
+                onChange={(e) => {
+                  setCupoMaximo(e.target.value);
+                }}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>E-mail responsable</FormLabel>
+              <Input
+                size="sm"
+                name="emailResp"
+                value={emailResp}
+                onChange={(e) => {
+                  setEmailResp(e.target.value);
+                }}
+              />
+            </FormControl>
           </ModalBody>
+          <ModalFooter>
+            <Button
+              colorScheme="blue"
+              mr={3}
+              onClick={() => {
+                setModalCursos(!modalCursos);
+              }}
+            >
+              Cerrar
+            </Button>
+            <Button
+              colorScheme="green"
+              onClick={() => {
+                agregarCurso();
+              }}
+            >
+              guardar
+            </Button>
+          </ModalFooter>
         </ModalContent>
       </Modal>
-
       <Modal
         isOpen={modalConceptos}
         onClose={() => {
